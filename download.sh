db4e6ea925a35d467ec1985a4bab43448f8e9347
Nelson Chen <crazysim@gmail.com>
2009-04-05 03:20:33 -0700

    seems to work 

---
+++ download.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Grab PDF and make XML
+
+# DLG
+curl http://www.housing.ucsb.edu/dining/menu/dlg/ThisWeekMenu.pdf -o "$(dirname $0)/menus/this_week_dlg.pdf"
+curl http://www.housing.ucsb.edu/dining/menu/dlg/NextWeekMenu.pdf -o "$(dirname $0)/menus/next_week_dlg.pdf"
+
+# Ortega
+curl http://www.housing.ucsb.edu/dining/menu/ortega/ThisWeekMenu.pdf -o "$(dirname $0)/menus/this_week_ortega.pdf"
+curl http://www.housing.ucsb.edu/dining/menu/ortega/NextWeekMenu.pdf -o "$(dirname $0)/menus/next_week_ortega.pdf"
+
+# Portola
+curl http://www.housing.ucsb.edu/dining/menu/portola/ThisWeekMenu.pdf -o "$(dirname $0)/menus/this_week_portola.pdf"
+curl http://www.housing.ucsb.edu/dining/menu/portola/NextWeekMenu.pdf -o "$(dirname $0)/menus/next_week_portola.pdf"
+
+# Carillo
+curl http://www.housing.ucsb.edu/dining/menu/carrillo/ThisWeekMenu.pdf -o "$(dirname $0)/menus/this_week_carrillo.pdf"
+curl http://www.housing.ucsb.edu/dining/menu/carrillo/NextWeekMenu.pdf -o "$(dirname $0)/menus/next_week_carrillo.pdf"
+
+for i in menus/*.pdf; do
+	$HOME/bin/pdftohtml $i -i -xml
+done
